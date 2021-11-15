de0a820ce91bef1a3592e167064a0d91950f30d9
Veeru <immaculateempyrean@gmail.com>
2021-11-15 13:01:13 +0530

    indentation fixed.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -31,63 +31,63 @@ import KoiChoco from '../components/MainFeedWidget/finishedWidgets/LatestPosts/K
 import WhiteAlbum2 from '../components/MainFeedWidget/finishedWidgets/LatestPosts/WhiteAlbum2.vue'; 
 
 export default {
-  name: 'Home',
-  components:{
-    KoiChoco,
-    WhiteAlbum2
-  },
-  data(){
-      return{
-          latestPostDisplayCounter: 0,
-          latestPostDisplayCounterMax: 1,
-          MostViewedPostDisplayCounter: 0
-      }
-  },
-  methods:{
-      latestLeftArrowClicked(){
-        if(this.latestPostDisplayCounter > 0){
-            this.latestPostDisplayCounter = this.latestPostDisplayCounter - 0.6;
+    name: 'Home',
+    components:{
+        KoiChoco,
+        WhiteAlbum2
+    },
+    data(){
+        return{
+            latestPostDisplayCounter: 0,
+            latestPostDisplayCounterMax: 1,
+            MostViewedPostDisplayCounter: 0
         }
-      },
-      latestRightArrowClicked(){
-        if(this.latestPostDisplayCounter < this.latestPostDisplayCounterMax){
-            this.latestPostDisplayCounter = this.latestPostDisplayCounter + 0.6;
+    },
+    methods:{
+        latestLeftArrowClicked(){
+            if(this.latestPostDisplayCounter > 0){
+                this.latestPostDisplayCounter = this.latestPostDisplayCounter - 0.6;
+            }
+        },
+        latestRightArrowClicked(){
+            if(this.latestPostDisplayCounter < this.latestPostDisplayCounterMax){
+                this.latestPostDisplayCounter = this.latestPostDisplayCounter + 0.6;
+            }
+        },
+
+        animateIn(el){
+            let animatables = el.querySelectorAll('.animate__animated');
+            animatables.forEach(function(element){
+                element.classList.add("animate__fadeIn")
+            });
+        },
+        cleanUpAfterIn(el){
+            let animatables = el.querySelectorAll('.animate__animated');
+            animatables.forEach(function(element){
+                element.classList.remove("animate__fadeIn")
+            });
+        },
+        
+        animateOut(el){
+            let animatables = el.querySelectorAll('.animate__animated');
+            animatables.forEach(function(el){
+                el.classList.add("animate__fadeOut")
+            });
+        },
+        cleanUpAfterOut(el){
+            let animatables = el.querySelectorAll('.animate__animated');
+            animatables.forEach(function(el){
+                el.classList.remove("animate__fadeOut")
+            });
+        },
+
+        leaveCanceled(){
+            this.latestPostDisplayCounter = Math.trunc(this.latestPostDisplayCounter)
+        },
+        afterLeave(){
+            this.latestPostDisplayCounter = Math.round(this.latestPostDisplayCounter)
         }
-      },
-
-      animateIn(el){
-        let animatables = el.querySelectorAll('.animate__animated');
-        animatables.forEach(function(element){
-            element.classList.add("animate__fadeIn")
-        });
-      },
-      cleanUpAfterIn(el){
-        let animatables = el.querySelectorAll('.animate__animated');
-        animatables.forEach(function(element){
-            element.classList.remove("animate__fadeIn")
-        });
-      },
-      
-      animateOut(el){
-        let animatables = el.querySelectorAll('.animate__animated');
-        animatables.forEach(function(el){
-            el.classList.add("animate__fadeOut")
-        });
-      },
-      cleanUpAfterOut(el){
-        let animatables = el.querySelectorAll('.animate__animated');
-        animatables.forEach(function(el){
-            el.classList.remove("animate__fadeOut")
-        });
-      },
-
-      leaveCanceled(){
-          this.latestPostDisplayCounter = Math.trunc(this.latestPostDisplayCounter)
-      },
-      afterLeave(){
-          this.latestPostDisplayCounter = Math.round(this.latestPostDisplayCounter)
-      }
-  }
+    }
 }
 </script>
 
