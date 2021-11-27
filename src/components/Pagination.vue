6b299e6bef3d2d5ea934a797afe2066ff3975113
Veeru <immaculateempyrean@gmail.com>
2021-11-27 08:37:47 +0530

    pagination centered on the page.

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,24 +1,27 @@
 <template>
-    <n-pagination :page-count="pageCount" :on-update:page="searchPageUpdated">
-        
-        <template #prev>
-            <n-button icon-placement="left" type="primary">
-                <template #icon>
-                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17 12H3"></path><path d="M6 9l-3 3l3 3"></path><path d="M21 14h-4v-4h4z"></path></g></svg>
-                </template>
-                Previous Page
-            </n-button>
-        </template>
+    <div class="pagination">    
+        <n-pagination :page-count="pageCount" :on-update:page="searchPageUpdated">
+            
+            <template #prev>
+                <n-button icon-placement="left" type="primary">
+                    <template #icon>
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17 12H3"></path><path d="M6 9l-3 3l3 3"></path><path d="M21 14h-4v-4h4z"></path></g></svg>
+                    </template>
+                    Previous Page
+                </n-button>
+            </template>
 
-        <template #next>
-            <n-button icon-placement="right" type="primary">
-                <template #icon>
-                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M7 12h14"></path><path d="M18 15l3-3l-3-3"></path><path d="M3 10h4v4H3z"></path></g></svg>
-                </template>
-                Next Page
-            </n-button>
-        </template>
-    </n-pagination>
+            <template #next>
+                <n-button icon-placement="right" type="primary">
+                    <template #icon>
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M7 12h14"></path><path d="M18 15l3-3l-3-3"></path><path d="M3 10h4v4H3z"></path></g></svg>
+                    </template>
+                    Next Page
+                </n-button>
+            </template>
+            
+        </n-pagination>
+    </div>
 </template>
 
 <script>
@@ -44,7 +47,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    .n-pagination{
-        
+    .pagination{
+        display: flex;
+        justify-content: center;
+
+        margin: calc(var(--spacing-large) * 2);
     }
 </style>
